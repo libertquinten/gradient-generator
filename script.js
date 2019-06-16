230ab6ccc0a5b12c8519771087758f9022de2e9a
Quinten Libert <libert1quinten@gmail.com>
2019-06-16 16:49:33 +0200

    Adding copyable CSS code to easily use a background in own project.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const color1 = document.querySelector(".color1");
-const color2 = document.querySelector('.color2');
-const body = document.querySelector('.background')
+const color1           = document.querySelector(".color1");
+const color2           = document.querySelector('.color2');
+const CSSCode          = document.querySelector('.CSSCode');
+const CSSCodeContainer = document.querySelector('.CSSCodeContainer');
+const body             = document.querySelector('.background');
 
 const changeBackgroundColor = () =>{
-    body.style.background = `linear-gradient(to right,${color1.value},${color2.value})`;
+    body.style.background  = `linear-gradient(to right,${color1.value},${color2.value})`;
+    CSSCode.textContent    = `background: linear-gradient(to right, ${color1.value}, ${color2.value});`;
 }
 
 color1.addEventListener('input',changeBackgroundColor);
