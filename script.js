72565260c5b5e6b750d3f71f37a8ab6c8466dcfa
Quinten Libert <libert1quinten@gmail.com>
2019-06-16 18:23:49 +0200

    Adding feature to invert text color when user sets gradient to be completely black

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const color1           = document.querySelector(".color1");
 const color2           = document.querySelector('.color2');
 const CSSCode          = document.querySelector('.CSSCode');
-const CSSCodeContainer = document.querySelector('.CSSCodeContainer');
 const body             = document.querySelector('.background');
 
 const changeBackgroundColor = () =>{
+
+    /*  If user sets the gradient to be completely black,
+        change the text color to white to ensure readability
+    */
+    if((color1.value === '#000000') && (color1.value === '#000000')){
+
+        body.style.color = '#ffffff';
+    } else {
+
+        body.style.color = '#000000';
+    }
+
     body.style.background  = `linear-gradient(to right,${color1.value},${color2.value})`;
     CSSCode.textContent    = `background: linear-gradient(to right, ${color1.value}, ${color2.value});`;
 }
